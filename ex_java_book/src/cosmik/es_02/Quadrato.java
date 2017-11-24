4b4dc60bfe6b7d832ac07f6dd2cc1b6dfe4b55eb
Cos-Mik <cos-mik@libero.it>
2017-11-24 21:21:27 +0100

    Tanto per provare a committare

--- ex_java_book/src/cosmik/es_02/Quadrato.java
+++ ex_java_book/src/cosmik/es_02/Quadrato.java
@@ -1,7 +1,7 @@
 package cosmik.es_02;
 
 public class Quadrato {
-	public int altezza, larghezza;
+	public int lati; // hai raggggione
 	// il quadrato ha altezza e larghezza uguali, perche' usi due variabili?
-	public static inal int NUMERO_LATI = 4; // le costanti si dichiarano static
+	public static final /*avevi messo inal lol*/ int NUMERO_LATI = 4; // le costanti si dichiarano static
 }
--- ex_java_book/src/cosmik/es_04/Main.java
+++ ex_java_book/src/cosmik/es_04/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		for (char c = 'a'; c <= 'z';++c) {
 			//if (c == 'j' || c == 'l' || c == 'w' || c == 'x' || c == 'y') continue;
+			//nie volevo solo che ritornasse l'alfabeto italiano senza i caratteri stranieri, tutto qui
 			System.out.print(c+" ");
 		}
 	}
